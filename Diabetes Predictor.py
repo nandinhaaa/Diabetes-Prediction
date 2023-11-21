aae31c187dc16edb2296dc1ac324cecae026eca2
Gabriel Pala <73294128+gabriellpala@users.noreply.github.com>
2023-11-21 19:22:00 -0300

    Update Diabetes Predictor.py

--- Diabetes Predictor.py
+++ Diabetes Predictor.py
@@ -1,36 +1,38 @@
-# Importing essential libraries
+# Importa as bibliotecas necessárias
 import numpy as np
 import pandas as pd
 import pickle
 
-# Loading the dataset
+# Carrega o conjunto de dados sobre diabetes do arquivo CSV usando o pandas
 df = pd.read_csv('kaggle_diabetes.csv')
 
-# Renaming DiabetesPedigreeFunction as DPF
+# Renomeia a coluna 'DiabetesPedigreeFunction' para 'DPF'
 df = df.rename(columns={'DiabetesPedigreeFunction':'DPF'})
 
-# Replacing the 0 values from ['Glucose','BloodPressure','SkinThickness','Insulin','BMI'] by NaN
+# Cria uma cópia profunda do DataFrame original para evitar alterações indesejadas
 df_copy = df.copy(deep=True)
+
+# Substitui os valores zero por NaN nas colunas específicas
 df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0,np.NaN)
 
-# Replacing NaN value by mean, median depending upon distribution
+# Preenche os valores NaN com média ou mediana das respectivas colunas
 df_copy['Glucose'].fillna(df_copy['Glucose'].mean(), inplace=True)
 df_copy['BloodPressure'].fillna(df_copy['BloodPressure'].mean(), inplace=True)
 df_copy['SkinThickness'].fillna(df_copy['SkinThickness'].median(), inplace=True)
 df_copy['Insulin'].fillna(df_copy['Insulin'].median(), inplace=True)
 df_copy['BMI'].fillna(df_copy['BMI'].median(), inplace=True)
 
-# Model Building
+# Divide o conjunto de dados em conjuntos de treinamento e teste usando train_test_split
 from sklearn.model_selection import train_test_split
 X = df.drop(columns='Outcome')
 y = df['Outcome']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
 
-# Creating Random Forest Model
+# Cria um modelo de classificação RandomForest com 20 estimadores e o treina com os dados de treinamento
 from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators=20)
 classifier.fit(X_train, y_train)
 
-# Creating a pickle file for the classifier
+# Salva o modelo treinado em um arquivo usando a biblioteca pickle
 filename = 'diabetes-prediction-rfc-model.pkl'
 pickle.dump(classifier, open(filename, 'wb'))
